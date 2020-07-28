873e7ce3a923c47b41e3e26615f7f89441b2a2b6
Johnson Lin <konglong_1984@msn.com>
2020-07-28 19:30:36 +1000

    change API to firebase

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,11 +1,9 @@
 // APIs
-export const API_KEY = 'qnSABWkWm0qUXFXcLuplMXGXGgizrKfj';
-
 export const WORKS_API = 'https://public-api.wordpress.com/rest/v1.1/sites/johnsonlinportfolio.wordpress.com/posts/?number=50';
 
-export const SKILLS_API = 'https://johnsonlin-service.herokuapp.com/skills';
+export const SKILLS_API = 'https://asia-northeast1-johnsonlin-service.cloudfunctions.net/app/skills';
 
-export const SEND_MESSAGE_API = 'https://johnsonlin-service.herokuapp.com/contact';
+export const SEND_MESSAGE_API = 'https://asia-northeast1-johnsonlin-service.cloudfunctions.net/app/contact';
 
 export const CAPTCHA_KEY = '6LeFnSkUAAAAAPNv1DcQ3TOWRfSrCo9Zb41kqQqh';
 
